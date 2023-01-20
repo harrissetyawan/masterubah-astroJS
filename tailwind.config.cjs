f8b590026a337aec0a37a8d1d1cebd8b575c70fd
Harris Setyawan <harrisfm90@gmail.com>
2023-01-20 15:15:11 +0700

    fisrt, but progress

---
+++ tailwind.config.cjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				SansPro: ['SrcSansPro', 'sans-serif'],
+			}
+		},
+		screens: {
+			'xs': '375px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		},
+	},
+	plugins: [],
+}
